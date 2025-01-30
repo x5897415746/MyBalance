0af0dbd52cc551bfcf2738b9d706462fac734954
x5897415746 <foss4fun@outlook.com>
2025-01-30 13:27:49 +0800

    Add basic reader logic in MainActivity

--- app/src/main/java/io/github/x5897415746/mybalance/MainActivity.java
+++ app/src/main/java/io/github/x5897415746/mybalance/MainActivity.java
@@ -1,12 +1,17 @@
 package io.github.x5897415746.mybalance;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -15,6 +20,7 @@ import io.github.x5897415746.mybalance.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "MainActivity";
     private ActivityMainBinding binding;
 
     @Override
@@ -28,12 +34,78 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications).build();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        if (getIntent() != null) {
+            handleIntent(getIntent());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableNfcReading();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableNfcReading();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(@NonNull Intent intent) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction()) || NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            Log.w(LOG_TAG, intent.getAction());
+            Tag nfcTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Log.w(LOG_TAG, "Found a tag: " + bytesToHex(nfcTag.getId()));
+            Log.w(LOG_TAG, nfcTag.toString());
+        }
+    }
+
+    private boolean enableNfcReading() {
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Log.w(LOG_TAG, "NFC Adapter is not available");
+            return false;
+        }
+        if (!nfcAdapter.isEnabled()) {
+            Log.w(LOG_TAG, "NFC is off");
+            return false;
+        }
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                PendingIntent.FLAG_MUTABLE);
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        return true;
+    }
+
+    private void disableNfcReading() {
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            hexString.append(String.format("%02X", b));
+        }
+        return hexString.toString();
     }
 
 }
